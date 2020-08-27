09be030c18f4e762c98fd6d5dab09903e3ed5da4
putridwijs <putridjs97@gmail.com>
2020-08-27 22:31:17 +0700

    Add all my files

---
+++ src/test/java/com/putri/com/putri/genbe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putri.com.putri.genbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
